4df7f6ae8ed76d8e11de2269a1900d8a29edf19e
Gabriel Teodoro Kuss <gabriel121212559@gmail.com>
2026-02-04 15:44:23 -0300

    Movi o vercel.json para a raiz

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m#o-fm@z+dh8b+afnwe_m-c08rm=!7s(gdxa^=4)%**mwp=+pu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', 'localhost', '127.0.0.1']
 
 
 # Application definition
